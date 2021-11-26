f172553ba0dec1f4f7b9c33542ab15587d2f158c
Tummas Joensen <tj@styr.fo>
2021-11-26 07:46:26 +0000

    Add project files.

---
+++ NinjaCore/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace NinjaCore
+{
+    class Program
+    {
+        private static string filename;
+        private static string newStartRange;
+        private static string newEndRange;
+        private static int startNr;
+        private static int endNr;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NinjaCore -path \"path to app.json\" -startRange 50000 -endRange 50050");
+            if(args.Length > 0)
+            {
+                for(var i = 0; i < args.Length; i++)
+                {
+                    if(args[i] == "-path")
+                    {
+                        filename = args[i + 1];
+
+                        if (!File.Exists(filename))
+                        {
+                            throw new Exception("Invalid filename!");
+                        }
+                    }
+                    if (args[i] == "-startRange")
+                    {
+                        newStartRange = args[i + 1];
+                        if (!Int32.TryParse(newStartRange, out startNr))
+                        {
+                            throw new Exception("Start range is invalid");
+                        }
+                    }
+                    if (args[i] == "-endRange")
+                    {
+                        newEndRange = args[i + 1];
+                        if (!Int32.TryParse(newEndRange, out endNr))
+                        {
+                            throw new Exception("End range is invalid");
+                        }
+                    }
+                }
+                Console.WriteLine("NinjaCore -path " + filename + " -startRange " + newStartRange + " -endRange " + newEndRange);
+                new Renamer().RenameFiles(filename, startNr, endNr);
+            }
+            else
+            {
+                Console.WriteLine("Please enter correct parameters!");
+            }
+
+        }
+    }
+}
---
+++ NinjaCore/Renamer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NinjaCore
+{
+    public class Renamer
+    {
+        private Dictionary<string, string> dictRange = new Dictionary<string, string>();
+        private string currentOldKey;
+        private string currentNewKey;
+        private int incidents = 0;
+        private string dir;
+        private int oldStartRange = 0;
+        private int oldEndRange = 0;
+        private string filename = "";
+        private int newStartRange;
+        private int newEndRange;
+
+        private void RenameAllIdsInFiles()
+        {
+            var directories = Directory.GetDirectories(dir);
+
+            foreach (var pair in dictRange)
+            {
+                currentOldKey = pair.Key;
+                currentNewKey = pair.Value;
+                ReadDirectories(directories);
+            }
+
+            var newString = "";
+            using (var streamR = new StreamReader(filename))
+            {
+
+                var text = streamR.ReadToEnd();
+                newString = text;
+                newString = text.Replace(oldStartRange.ToString(), newStartRange.ToString());
+                newString = newString.Replace(oldEndRange.ToString(), newEndRange.ToString());
+            }
+            using (var streamW = new StreamWriter(filename))
+            {
+                if (newString.Length > 0)
+                {
+                    streamW.Write(newString);
+                }
+            }
+        }
+
+        private void ReadDirectories(string[] directories)
+        {
+            foreach (var dir in directories)
+            {
+                var files = Directory.GetFiles(dir);
+                foreach (var file in files)
+                {
+                    SearchAndReplaceInFile(file);
+
+                }
+                ReadDirectories(Directory.GetDirectories(dir));
+            }
+        }
+
+        private void SearchAndReplaceInFile(string file)
+        {
+            var oldText = "";
+            var newString = "";
+            using (var streamR = new StreamReader(file))
+            {
+
+                oldText = streamR.ReadToEnd();
+                newString = oldText;
+                if (oldText.Contains(currentOldKey))
+                    newString = oldText.Replace(currentOldKey, currentNewKey);
+            }
+            using (var streamW = new StreamWriter(file))
+            {
+                if (!newString.Equals(oldText))
+                {
+                    streamW.Write(newString);
+                    incidents += 1;
+                }
+            }
+        }
+
+        public void RenameFiles(string filePath, int startRange, int endRange)
+        {
+            filename = filePath;
+            ReadAppFile();
+            var oldStart = oldStartRange;
+            var oldEnd = oldEndRange;
+            newStartRange = startRange;
+            newEndRange = endRange;
+            for (var i = oldStart; i < oldEnd + 1; i++)
+            {
+                dictRange.Add(i.ToString(), newStartRange.ToString());
+                newStartRange += 1;
+            }
+
+            RenameAllIdsInFiles();
+
+            Console.WriteLine("changed " + incidents + " id's");
+            Console.WriteLine("including app.json");
+            Console.WriteLine("Remember to generate new permissionset in AL");
+        }
+
+        private void ReadAppFile()
+        {
+            if (!string.IsNullOrWhiteSpace(filename) && filename.EndsWith("app.json"))
+            {
+                dir = Path.GetDirectoryName(filename);
+                dir = dir + "\\src";
+
+                using (var streamR = new StreamReader(filename))
+                {
+                    while (!streamR.EndOfStream)
+                    {
+                        var json = streamR.ReadLine();
+                        if (json.Contains("\"from\":"))
+                        {
+                            string fromId = json.Trim().Split(':').Last().Replace(',', ' ').Trim();
+                            oldStartRange = Int32.Parse(fromId);
+                            Console.WriteLine("Old start range: " + oldStartRange.ToString());
+                        }
+                        if (json.Contains("\"to\":"))
+                        {
+                            string toId = json.Trim().Split(':').Last();
+                            oldEndRange = Int32.Parse(toId);
+                            Console.WriteLine("Old end range: " + oldEndRange.ToString());
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
